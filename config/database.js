a72d9f407fc35a84209b0113be37539f20f3844a
luizalinda <84139409+luizalinda@users.noreply.github.com>
2022-04-29 07:50:29 -0300

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const conexao = async() => {
+    //conexao local
+    //var bdlocal = await mongoose.connect('mongodb://localhost/ac1tri')
+    //conexao atlas
+    var atlas = await mongoose.connect('mongodb+srv://userAdmin:Lu0612@fiaptecnico.0ptot.mongodb.net/test')
+}
+
+module.exports = conexao
+
